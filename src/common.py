ba0339f3bad2cb8c75941ecebdd962f82a97f000
Mohammad <master.mhn1388@gmail.com>
2021-02-10 18:58:37 +0330

    add segmentation color info to common.py

--- src/common.py
+++ src/common.py
@@ -32,13 +32,32 @@ CONVERTER = np.uint8([
 
 
 COLOR = np.uint8([
-        (  0,   0,   0),    # unlabeled
-        (220,  20,  60),    # ped
-        (157, 234,  50),    # road line
-        (128,  64, 128),    # road
-        (244,  35, 232),    # sidewalk
-        (  0,   0, 142),    # car
-        (255,   0,   0),    # red light
-        (255, 255,   0),    # yellow light
-        (  0, 255,   0),    # green light
+    (0, 0, 0),  # 0=Unlabeled
+    # 1=Building, 2=Fence, 3=Other   , 4=Pedestrian, 5=Pole
+    (70, 70, 70),
+    (100, 40, 40),
+    (55, 90, 80),
+    (220, 20, 60),
+    (153, 153, 153),
+    # 6=RoadLine, 7=Road, 8=SideWalk, 9=Vegetation, 10=Vehicles
+    (157, 234, 50),
+    (128, 64, 128),
+    (244, 35, 232),
+    (107, 142, 35),
+    (0, 0, 142),
+    # 11=Wall, 12=TrafficSign, 13=Sky, 14=Ground, 15=Bridge
+    (102, 102, 156),
+    (220, 220, 0),
+    (70, 130, 180),
+    (81, 0, 81),
+    (150, 100, 100),
+    # 16=RailTrack, 17=GuardRail, 18=TrafficLight, 19=Static, 20=Dynamic
+    (230, 150, 140),
+    (180, 165, 180),
+    (250, 170, 30),
+    (110, 190, 160),
+    (170, 120, 50),
+    # 21=water, 22=terrain
+    (45, 60, 150),
+    (145, 170, 100),
         ])
